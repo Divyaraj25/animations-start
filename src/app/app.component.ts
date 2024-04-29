3299c9b93a5c1d58d5887437d574878ec699a15a
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-29 09:01:51 +0530

    transition with shrink button working done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,38 @@ import { Component } from '@angular/core';
       })),
       transition('normal <=> highlighted', animate(300)),
       // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(50px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal'
+  wildState = 'normal'
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate(){
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrunken(){
+    this.wildState = 'shrunken'
   }
 
   onAdd(item) {
